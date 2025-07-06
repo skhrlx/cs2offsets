e5dddcc0184cdab12b21a87c1b1b3d057fb5c188
skhrlx <85849948+skhrlx@users.noreply.github.com>
2025-07-05 19:26:54 -0700

    Add files via upload

--- output/animationsystem_dll.cs
+++ output/animationsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-06-11 19:29:54.262070500 UTC
+// 2025-07-06 02:24:11.946426500 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: animationsystem.dll
